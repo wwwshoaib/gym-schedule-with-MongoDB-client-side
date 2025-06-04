5c98315859dfbe2f5f0cf02554ca844c042d12f7
Shoaib <shoaibchemistrysust@gmail.com>
2025-06-04 10:39:39 +0600

    search system has been implemented

--- src/pages/Schedule/Schedule.jsx
+++ src/pages/Schedule/Schedule.jsx
@@ -1,13 +1,25 @@
 import { useLoaderData } from "react-router-dom";
 import ScheduleTable from "../../components/SchedleTable";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Schedule = () => {
   const data = useLoaderData();
   //state declaration for updating data on the UI after the delete operation
-  
   const [scheduleData, setScheduleData] = useState(data)
-  console.log(scheduleData);
+  //console.log(scheduleData);
+  
+  //state declaration for  search
+  const [search, setSearch] = useState("");
+  // console.log(search);
+  useEffect(() => {
+    fetch(`http://localhost:8800/search?searchParams=${search}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setScheduleData(data);
+      })
+
+  }, [search]) 
   return (
     <>
       <div className="w-[400px] mx-auto mb-4">
@@ -35,19 +47,21 @@ const Schedule = () => {
               </tr>
             </thead>
             <tbody>
-              {
-                scheduleData.length === 0?  <p>No data found!</p>
-                : scheduleData.map((schedule, index) => (
+              {scheduleData.length === 0 ? (
+                <tr>
+                  <td colSpan="6" className="text-center">No data found!</td>
+                </tr>
+              ) : (
+                scheduleData.map((schedule, index) => (
                   <ScheduleTable
-                  key = {schedule?._id}
-                  idx = {index}
-                  schedule = {schedule}
-                  scheduleData = {scheduleData}
-                  setScheduleData = {setScheduleData}
-                  > </ScheduleTable>
-
+                    key={schedule?._id}
+                    idx={index}
+                    schedule={schedule}
+                    scheduleData={scheduleData}
+                    setScheduleData={setScheduleData}
+                  />
                 ))
-              }
+              )}
             </tbody>
           </table>
         </div>
